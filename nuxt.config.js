431c289083c1090987f89dd3d7555ad3d5c720c3
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2023-12-05 17:41:18 +0500

    добавлена папка assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,27 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    './assets/scss/main.scss'
+  ],
 
-  // css: ['vue-slider-component/dist-css/vue-slider-component.css'],
+  styleResources: {
+    scss: [
+      './assets/scss/vars/index.scss',
+      './assets/scss/mixins/index.scss'
+    ]
+  },
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -39,18 +47,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   env: {
     API_URL: process.env.API_URL
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 };
