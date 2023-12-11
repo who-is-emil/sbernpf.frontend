bf4e493bc993f4e928caa93ad7eac165362a654f
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2023-12-11 15:00:19 +0500

    ssr: false

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  ssr: true,
+  ssr: false,
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -77,7 +77,6 @@ export default {
   build: {},
 
   router: {
-    // base: '/sbernpf.frontend/dist/'
     base: process.env.NODE_ENV === 'production' ? '/sbernpf.frontend/dist/' : '/'
   },
 
