1f3911cc494f38e7a97ad51dd9c4d04a78f6faa0
emiltukhvatullin <e.tuhvatullin@uplab.ru>
2024-02-14 11:50:42 +0500

    text fix

--- components/AppSteps/AppSteps.vue
+++ components/AppSteps/AppSteps.vue
@@ -52,7 +52,7 @@ export default {
       },
       items: [
         {
-          text: 'Заключите договор<br>со&nbsp;СберНПФ в 2024 году'
+          text: 'Заключите договор<br>со&nbsp;СберНПФ в 2024 году<br>Онлайн или <a href="https://www.sberbank.com/ru/oib?tab=vsp" target="_blank" rel="nofollow">в&nbsp;отделениях&nbsp;Сбербанка.</a>'
         },
         {
           text: 'Сделайте взнос на&nbsp;сумму от&nbsp;2&nbsp;000 ₽. Делайте взносы регулярно'
